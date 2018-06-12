a1b2031689b24e4fc8b74addaf9cc271a0bfe091
metalm <felipetiagodecarli@hotmail.com>
2018-06-12 02:01:12 -0300

    add print_row func

--- main.c
+++ main.c
@@ -27,8 +27,7 @@ Matrix *init_matrix (uint64_t m, uint64_t n) {
 	Matrix *mat = malloc (sizeof (Matrix));
 	mat->m = m;
 	mat->n = n;
-	mat->elem = malloc(sizeof(Elem));
-	mat->elem->next = NULL;
+	mat->elem = malloc (sizeof (Elem));
 	return mat;
 }
 
@@ -46,6 +45,11 @@ void freeMatrix (Matrix *mat) {
 }
 
 
+int cmpfunc (const void * a, const void * b) {
+   return ( *(uint64_t*)a - *(uint64_t*)b );
+}
+
+
 float get_element (Matrix *mat, uint64_t m, uint64_t n) {
 	Elem *node = mat->elem;
 	while (node != NULL) {
@@ -60,34 +64,31 @@ float get_element (Matrix *mat, uint64_t m, uint64_t n) {
 
 void add_element (Matrix *mat, uint64_t m, uint64_t n, float val) {
 	Elem *node = mat->elem;
-	// Encontra ultimo elemento da lista
 	while (node->next != NULL) {
 		node = node->next;
 	}
-	// Aloca memoria e adiciona elemento
 	node->next = malloc (sizeof (Elem));
 	node = node->next;
-	node->next = NULL;
 	node->row_idx = m;
 	node->col_idx = n;
 	node->val = val;
+	node->next = NULL;
 }
 
 
 
-uint64_t get_not_null_num (Elem *elem) {
+static uint64_t get_not_null_num (Elem *elem) {
 	Elem *node = elem;
 	bool got_row;
 	uint64_t *not_null = malloc (sizeof (uint64_t));
 	uint64_t not_null_num = 0;
 
-	while (node != NULL) {
-
+	while (node->next != NULL) {
+		node = node->next;
 		got_row = false;
 
 		for (int i = 0; i < not_null_num; ++i) {
 			if (node->row_idx == not_null[i]) {
-				node = node->next;
 				got_row = true;
 				break;
 			}
@@ -95,29 +96,82 @@ uint64_t get_not_null_num (Elem *elem) {
 
 		if (got_row) continue;
 		not_null[not_null_num++] = node->row_idx;
-		not_null = realloc (not_null, sizeof(uint64_t) * not_null_num);
-		node = node->next;
+		not_null = realloc (not_null, sizeof(uint64_t) * (not_null_num + 1));
 	}
 
-	for (int i = 0; i < not_null_num; ++i) {
-		printf ("%lu ", not_null[i]);
-	}
-	printf ("\n");
-
-	return 0;
+	return not_null_num;
 
 }
+
+
+static uint64_t *get_not_null (Elem *elem) {
+	Elem *node = elem;
+	bool got_row;
+	uint64_t *not_null = malloc (sizeof (uint64_t));
+	uint64_t not_null_num = 0;
+
+	while (node->next != NULL) {
+		node = node->next;
+		got_row = false;
+
+		for (int i = 0; i < not_null_num; ++i) {
+			if (node->row_idx == not_null[i]) {
+				got_row = true;
+				break;
+			}
+		}
+
+		if (got_row) continue;
+		not_null[not_null_num++] = node->row_idx;
+		not_null = realloc (not_null, sizeof(uint64_t) * (not_null_num + 1));
+	}
+
+	return not_null;
+
+}
+
+
+float display_rows_sum (Matrix *mat) {
+	uint64_t *not_null_row = get_not_null (mat->elem);
+	uint64_t not_null_sz = get_not_null_num (mat->elem), row_idx;
+	float hold_sum = 0;
+	Elem *node = mat->elem;
+
+	qsort(not_null_row, not_null_sz, sizeof(uint64_t), cmpfunc);
+
+	for (row_idx = 0; row_idx < not_null_sz; ++row_idx) {
+		while (node->next != NULL) {
+			node = node->next;
+			if (node->row_idx == not_null_row[row_idx]) {
+				hold_sum += node->val;
+			}
+		}
+		printf ("Linha %lu - Soma = %f\n", not_null_row[row_idx], hold_sum);
+		hold_sum = 0;
+		node = mat->elem;
+	}
+}
+
+
+void print_non_null (Matrix *mat) {
+	Elem *node = mat->elem;
+	while (node->next != NULL) {
+		node = node->next;
+		printf ("%f\n", node->val);
+	}
+}
+
 
 
 int main (void) {
 
 	Matrix *mat = init_matrix (20, 20);
-	add_element (mat, 10, 5, 23);
-	add_element (mat, 2, 5, 21);
-
-	get_not_null_num (mat->elem);
-
-	//printf ("%f\n", get_element (mat, 0, 0));
+	add_element (mat, 0, 1, 2.2);
+	add_element (mat, 8, 5, 2.3);
+	add_element (mat, 1, 2, 22.1);
+	//print_non_null (mat);
+	display_rows_sum (mat);
+	//printf ("%lu\n", get_not_null_num (mat->elem));
 
 	return 0;
 }
