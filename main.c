2bf8a3048ade528557376a77cb492e4783a9a2a9
metalm <felipetiagodecarli@hotmail.com>
2018-06-12 02:26:38 -0300

    add comments

--- main.c
+++ main.c
@@ -2,34 +2,38 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include "main.h"
 
-/*
- *
- *	A matriz sera inicializada utilizando a estrutura DOK
- *	Veja: https://en.wikipedia.org/wiki/Sparse_matrix
- *	
+/**
+  * @desc A matriz sera inicializada utilizando a estrutura DOK,
+  * como eh sugerido em https://en.wikipedia.org/wiki/Sparse_matrix#Storing_a_sparse_matrix
+  *
+  * @param long unsined $m - guarda o numero de linhas da matriz
+  * 	   long unsigned $n - guarda o numero de colunas da matriz
+  *
+  * @return mat - retorna a matrix inicializada ou NULL caso a alocacao falhe
 */
 
-struct elem {
-	uint64_t row_idx, col_idx;
-	float val;
-	struct elem *next;
-}; typedef struct elem Elem;
-
-
-struct matrix {
-	uint64_t m, n;
-	Elem *elem;
-}; typedef struct matrix Matrix;
-
-
 Matrix *init_matrix (uint64_t m, uint64_t n) {
+
 	Matrix *mat = malloc (sizeof (Matrix));
+	mat->elem = malloc (sizeof (Elem));
+
+	if (mat == NULL || mat->elem == NULL) return NULL;
+
+	// Else
 	mat->m = m;
 	mat->n = n;
-	mat->elem = malloc (sizeof (Elem));
 	return mat;
 }
+
+
+/**
+  * @desc Apaga a matriz percorrendo todos os elementos adicionados
+  *
+  * @param Matrix *mat - matriz a ser apagada
+  *
+*/
 
 void freeMatrix (Matrix *mat) {
 	// Apaga todos os elementos da matriz
@@ -45,15 +49,32 @@ void freeMatrix (Matrix *mat) {
 }
 
 
+/**
+  * @desc Compara valor de 'a' e 'b' para a func qsort()
+  *
+  * @param const void * a, const void * b - valores a serem comparados
+  *
+*/
+
 int cmpfunc (const void * a, const void * b) {
    return ( *(uint64_t*)a - *(uint64_t*)b );
 }
 
 
-float get_element (Matrix *mat, uint64_t m, uint64_t n) {
+/**
+  * @desc Retorna um valor na posicao (i, j) da matriz
+  *
+  * @param Matrix *mat - matriz a ser consultada
+  *		   long unsigned i, j - coordenadas do elemento a ser consultado
+  *
+  *	@return node->val se elemento for nao nulo ou 0 se for nulo
+  *
+*/
+
+float get_element (Matrix *mat, uint64_t i, uint64_t j) {
 	Elem *node = mat->elem;
 	while (node != NULL) {
-		if (node->row_idx == m && node->col_idx == n) {
+		if (node->row_idx == i && node->col_idx == j) {
 			return node->val;
 		}
 		node = node->next;
@@ -61,6 +82,15 @@ float get_element (Matrix *mat, uint64_t m, uint64_t n) {
 	return 0;
 }
 
+
+/**
+  * @desc Adiciona um elemento na posicao (m,n) da matriz
+  *
+  * @param Matrix *mat - matriz a ser consultada
+  *		   long unsigned m, n - coordenadas do elemento a ser consultado
+  *		   float val - valor a ser adicionado na matriz
+  *
+*/
 
 void add_element (Matrix *mat, uint64_t m, uint64_t n, float val) {
 	Elem *node = mat->elem;
@@ -76,8 +106,82 @@ void add_element (Matrix *mat, uint64_t m, uint64_t n, float val) {
 }
 
 
+/**
+  * @desc Retorna um vetor de long unsined contendo a posicao de colunas
+  *		  com elementos nao nulos
+  *
+  * @param Elem *elem - Elementos da matriz
+  *
+  * @return long unsigned *not_null - vetor com posicoes de cols nao nulas
+  *
+*/
 
-static uint64_t get_not_null_num (Elem *elem) {
+static uint64_t *get_not_null_col (Elem *elem) {
+	Elem *node = elem;
+	bool got_col;
+	uint64_t *not_null = malloc (sizeof (uint64_t));
+	uint64_t not_null_num = 0;
+
+	while (node->next != NULL) {
+		node = node->next;
+		got_col = false;
+
+		for (int i = 0; i < not_null_num; ++i) {
+			if (node->col_idx == not_null[i]) {
+				got_col = true;
+				break;
+			}
+		}
+
+		if (got_col) continue;
+		not_null[not_null_num++] = node->col_idx;
+		not_null = realloc (not_null, sizeof(uint64_t) * (not_null_num + 1));
+	}
+
+	return not_null;
+
+}
+
+
+/**
+  * @desc Retorna a quantidade de colunas com elementos nao nulos
+  *
+  * @param Elem *elem - Elementos da matriz
+  *
+  * @return long unsigned not_null_num - numero de colunas nao nulas
+  *
+*/
+
+static uint64_t get_not_null_col_num (Elem *elem) {
+	Elem *node = elem;
+	bool got_col;
+	uint64_t *not_null = malloc (sizeof (uint64_t));
+	uint64_t not_null_num = 0;
+
+	while (node->next != NULL) {
+		node = node->next;
+		got_col = false;
+
+		for (int i = 0; i < not_null_num; ++i) {
+			if (node->col_idx == not_null[i]) {
+				got_col = true;
+				break;
+			}
+		}
+
+		if (got_col) continue;
+		not_null[not_null_num++] = node->col_idx;
+		not_null = realloc (not_null, sizeof(uint64_t) * (not_null_num + 1));
+	}
+
+	return not_null_num;
+
+}
+
+
+/* Veja l109 */
+
+static uint64_t get_not_null_row_num (Elem *elem) {
 	Elem *node = elem;
 	bool got_row;
 	uint64_t *not_null = malloc (sizeof (uint64_t));
@@ -104,7 +208,9 @@ static uint64_t get_not_null_num (Elem *elem) {
 }
 
 
-static uint64_t *get_not_null (Elem *elem) {
+/* Veja l146 */
+
+static uint64_t *get_not_null_row (Elem *elem) {
 	Elem *node = elem;
 	bool got_row;
 	uint64_t *not_null = malloc (sizeof (uint64_t));
@@ -131,9 +237,16 @@ static uint64_t *get_not_null (Elem *elem) {
 }
 
 
-float display_rows_sum (Matrix *mat) {
-	uint64_t *not_null_row = get_not_null (mat->elem);
-	uint64_t not_null_sz = get_not_null_num (mat->elem), row_idx;
+/**
+  * @desc Imprime na tela a soma dos elementos de linhas nao nulas
+  *
+  * @param Matrix *mat - matriz a ser consultada
+  *
+*/
+
+void display_rows_sum (Matrix *mat) {
+	uint64_t *not_null_row = get_not_null_row (mat->elem);
+	uint64_t not_null_sz = get_not_null_row_num (mat->elem), row_idx;
 	float hold_sum = 0;
 	Elem *node = mat->elem;
 
@@ -153,14 +266,28 @@ float display_rows_sum (Matrix *mat) {
 }
 
 
-void print_non_null (Matrix *mat) {
+/* Veja l241 */
+
+void display_cols_sum (Matrix *mat) {
+	uint64_t *not_null_col = get_not_null_col (mat->elem);
+	uint64_t not_null_sz = get_not_null_col_num (mat->elem), col_idx;
+	float hold_sum = 0;
 	Elem *node = mat->elem;
-	while (node->next != NULL) {
-		node = node->next;
-		printf ("%f\n", node->val);
+
+	qsort(not_null_col, not_null_sz, sizeof(uint64_t), cmpfunc);
+
+	for (col_idx = 0; col_idx < not_null_sz; ++col_idx) {
+		while (node->next != NULL) {
+			node = node->next;
+			if (node->col_idx == not_null_col[col_idx]) {
+				hold_sum += node->val;
+			}
+		}
+		printf ("Coluna %lu - Soma = %f\n", not_null_col[col_idx], hold_sum);
+		hold_sum = 0;
+		node = mat->elem;
 	}
 }
-
 
 
 int main (void) {
@@ -168,9 +295,10 @@ int main (void) {
 	Matrix *mat = init_matrix (20, 20);
 	add_element (mat, 0, 1, 2.2);
 	add_element (mat, 8, 5, 2.3);
-	add_element (mat, 1, 2, 22.1);
+	add_element (mat, 1, 1, 22.1);
 	//print_non_null (mat);
 	display_rows_sum (mat);
+	display_cols_sum (mat);
 	//printf ("%lu\n", get_not_null_num (mat->elem));
 
 	return 0;
